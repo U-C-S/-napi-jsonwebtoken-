d0af9fc38fc09470a08b93736eea98e9a4623982
U-C-S <ucs3721@outlook.com>
2022-04-19 00:24:33 +0530

    add test for comparing payload after sign and verify

--- tests/compare.ts
+++ tests/compare.ts
@@ -1,15 +1,16 @@
 import test from 'ava';
 import { sign as signjwt } from 'jsonwebtoken';
-import { JwtAlgorithm, sign } from '../index.js';
+import { JwtAlgorithm, sign, verify } from '../index.js';
+
+let payload = {
+  age: '20',
+  iat: Math.floor(Date.now() / 1000),
+  name: 'John Doe',
+  exp: 10000000000,
+};
+let secret = 'secret';
 
 test('Compare with jsonwebtoken npm package', (t) => {
-  let payload = {
-    age: '20',
-    iat: Math.floor(Date.now() / 1000),
-    name: 'John Doe',
-  };
-  let secret = 'secret';
-
   let node = signjwt(payload, secret, {
     algorithm: 'HS256',
   });
@@ -24,4 +25,25 @@ test('Compare with jsonwebtoken npm package', (t) => {
   );
 
   t.is(node, rust);
+});
+
+test('Sign and Verify the JWT', (t) => {
+  let jwtoken = sign(
+    {
+      alg: JwtAlgorithm.HS256,
+      typ: 'JWT',
+    },
+    JSON.stringify(payload),
+    secret,
+  );
+
+  let decodedInfo = verify(jwtoken, secret, {
+    alg: JwtAlgorithm.HS256,
+    leeway: 40,
+    validateExp: false,
+    validateNbf: false,
+  });
+  let x = JSON.parse(decodedInfo.claims).name;
+
+  t.is(payload.name, x);
 });
